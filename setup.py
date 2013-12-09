060c9874f850361389beb076ab94a3999d999e2b
Joshua Harlow <harlowja@yahoo-inc.com>
2013-12-08 17:09:23 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def _path(fn):
 
 
 setup(name='gerrit-view',
-      version='0.2.2',
+      version='0.2.3',
       description='Gerrit viewer tools',
       author="Joshua Harlow",
       author_email='harlowja@yahoo-inc.com',
