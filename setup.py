19258a9ebb9c059078a4f13666172d392b9c9ec4
Joshua Harlow <harlowja@yahoo-inc.com>
2014-02-20 13:58:20 -0800

    Bump to version 0.3.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def _path(fn):
 
 
 setup(name='gerrit-view',
-      version='0.2.5',
+      version='0.3.0',
       description='Gerrit viewer tools',
       author="Joshua Harlow",
       author_email='harlowja@yahoo-inc.com',
