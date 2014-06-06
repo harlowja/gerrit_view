03fa513a7a245fe4239a2be7ed0dec6d93e00db3
Joshua Harlow <harlowja@yahoo-inc.com>
2014-06-06 10:32:36 -0700

    Bump for 0.4.2

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ def _readme():
 
 
 setup(name='gerrit-view',
-      version='0.4.1',
+      version='0.4.2',
       description='Gerrit viewer tools',
       author="Joshua Harlow",
       author_email='harlowja@yahoo-inc.com',
